4125b6bb4a29d5b62f28b5a81df47b549e542d49
Ganten Xu <zjxu97@gmail.com>
2025-10-23 00:53:40 +0800

    fix kafka path

--- restart-in-kubernetes.sh
+++ restart-in-kubernetes.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 # restart-in-kubernetes.sh
-git add .
-git commit -m "Auto commit before restarting tethys in Kubernetes"
-mvn clean package -DskipTests
-./build-docker.sh
+
+# 检查代码是否有变动
+if git diff --quiet && git diff --staged --quiet; then
+    echo "代码没有变动，跳过构建步骤。"
+else
+    echo "检测到代码变动，开始构建..."
+    git add .
+    git commit -m "Auto commit before restarting tethys in Kubernetes"
+
+    mvn clean package -DskipTests
+    ./build-docker.sh
+fi
+
 kubectl delete pod -l app=tethys -n app
 echo "Restarted tethys pods in Kubernetes."
 echo "Use
