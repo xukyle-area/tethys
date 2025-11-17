30a0a06b1b42ec27e7945b14a996c8ffaefb7297
Ganten Xu <zjxu97@gmail.com>
2025-11-17 17:24:07 +0800

    reset project

--- src/main/java/com/ganten/tethys/kafka/config/KafkaConfig.java
+++
@@ -1,9 +0,0 @@
-package com.ganten.tethys.kafka.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
-
-@Configuration
-@EnableKafka
-public class KafkaConfig {
-}
