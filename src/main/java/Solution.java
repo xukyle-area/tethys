baed092a067b7a28c0763ca477d2d23ac969ac0b
Ganten Xu <zjxu97@gmail.com>
2026-01-20 14:47:11 +0800

    enhance code comments

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,21 +1,5 @@
-import java.util.LinkedList;
-
-class Solution {
-    public int minAddToMakeValid(String s) {
-        LinkedList<Character> stack = new LinkedList<>();
-
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
-                stack.push(c);
-            }
-            if (c == ')') {
-                if (stack.size() != 0 && stack.getFirst() == '(') {
-                    stack.pop();
-                } else {
-                    stack.push(c);
-                }
-            }
-        }
-        return stack.size();
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
     }
 }
