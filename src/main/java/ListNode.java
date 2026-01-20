7bd2eb1eddc8de04df4979316b549704c88eacdb
Ganten Xu <zjxu97@gmail.com>
2026-01-20 15:07:57 +0800

    enhance code comments for Sliding Window

--- src/main/java/ListNode.java
+++ src/main/java/ListNode.java
@@ -2,6 +2,13 @@ class ListNode {
     int val;
     ListNode next;
 
+    ListNode() {}
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
     ListNode(int x) {
         val = x;
         next = null;
--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,5 +1,23 @@
-public class Solution {
-    public ListNode detectCycle(ListNode head) {
-        
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode virtualNode = new ListNode();
+        ListNode cur = virtualNode;
+        ListNode cur1 = list1;
+        ListNode cur2 = list2;
+
+        while (true) {
+
+        }
     }
 }
