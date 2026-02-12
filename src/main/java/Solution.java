871c89574d0c484719f7f21396fc14c8ea931109
Ganten Xu <zjxu97@gmail.com>
2026-02-12 12:11:33 +0800

    add some tree problems

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,10 +1,22 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-        List<Integer> list = new ArrayList<>();
+    int result = 0;
 
-        return list;
+    public int sumOfLeftLeaves(TreeNode root) {
+        this.helper(root, false);
+        return result;
+    }
+
+    private void helper(TreeNode node, boolean isLeft) {
+        if (node == null) {
+            return;
+        }
+        if (node.left == null && node.right == null) {
+            if (isLeft) {
+                result += node.val;
+            }
+            return;
+        }
+        this.helper(node.left, true);
+        this.helper(node.right, false);
     }
 }
